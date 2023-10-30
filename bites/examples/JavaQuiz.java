97668c01fe237f8132915eb691fcfdd14d250bc5
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-30 15:39:28 +0000

    Add JavaQuiz

---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,14 @@
+package bites.examples;
+
+public class JavaQuiz {
+    public static void main(String[] args) {
+        System.out.println("1. Does all Java code have to be inside a class?");
+        System.out.println("Yes");
+        System.out.println("2. What is compilation?");
+        System.out.println("The process of converting high-level Java code into lower-level Byte code (that the processor understands) prior to execution");
+        System.out.println("3. What is special about the main method?");
+        System.out.println("There is only one main method in a program and it is needed for the program to compile. It's called automatically when the program is executed");
+        System.out.println("4. What does void mean?");
+        System.out.println("Declares that the method returns nothing");
+    }
+}
--- bites/examples/Message.java
+++ bites/examples/Message.java
@@ -3,5 +3,10 @@ package bites.examples;
 class Message {
     public static void main(String[] args) {
         System.out.println("Hello? Is there anyone out there?");
+        System.out.println(args[0]);
+        Message.hello();
+    }
+    public static void hello() {
+        System.out.println("Hello!");
     }
 }
