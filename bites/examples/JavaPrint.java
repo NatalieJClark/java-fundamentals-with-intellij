8bfe40b99eb8b5be96be1fd61ad89f49a45a0264
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-30 15:50:37 +0000

    Add JavaPrint

---
+++ bites/examples/JavaPrint.java
@@ -0,0 +1,9 @@
+package bites.examples;
+
+public class JavaPrint {
+    public static void main(String[] args) {
+        System.out.println(args);
+        System.out.println("This method is expecting an array '[L' of strings 'java.lang.String' as an argument");
+        System.out.println("The hexadecimal hash code is reference to the memory location of the array");
+    }
+}
--- bites/examples/Message.java
+++ bites/examples/Message.java
@@ -3,7 +3,7 @@ package bites.examples;
 class Message {
     public static void main(String[] args) {
         System.out.println("Hello? Is there anyone out there?");
-        System.out.println(args[0]);
+        System.out.println(args);
         Message.hello();
     }
     public static void hello() {
