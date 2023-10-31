9d0e255e96c27165cdc52da784986a07627b924a
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-31 13:33:56 +0000

    Add Quiz

---
+++ bites/examples/Quiz.java
@@ -0,0 +1,30 @@
+package bites.examples;
+
+public class Quiz {
+    // Class attributes
+    String questionOne;
+    String questionTwo;
+
+    // Constructor
+    public Quiz(String questionOne, String questionTwo){
+        this.questionOne = questionOne;
+        this.questionTwo = questionTwo;
+    }
+
+    // Getter methods
+    public String getQuestionOne(){
+        return this.questionOne;
+    }
+
+    public String  getQuestionTwo(){
+        return this.questionTwo;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Quiz myQuiz = new Quiz("what is the capital of Burkina Faso?", "What is the capital of Bhutan?");
+        System.out.println(myQuiz.getQuestionOne());
+        System.out.println(myQuiz.getQuestionTwo());
+    }
+
+}
