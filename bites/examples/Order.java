4bae539e4d26441078ea998a3dca6812f4473d24
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-31 18:13:36 +0000

    Add friendlyTotal()

--- bites/examples/Order.java
+++ bites/examples/Order.java
@@ -2,15 +2,22 @@ package bites.examples;
 
 public class Order {
     // Class attributes:
-    Integer total = 0;
+    Integer total;
+
+    // Constructor:
+    public Order(Integer total){
+        this.total = total;
+    }
 
     // Main:
     public static void main(String[] args) {
-        Order order = new Order();
+        Order order = new Order(0);
         order.addAmount(20);
         order.addDiscount(5);
         Integer total = order.getTotal();
         System.out.println(total);
+        String friendlyTotal = order.friendlyTotal();
+        System.out.println(friendlyTotal);
     }
 
     // Instance methods:
@@ -20,6 +27,10 @@ public class Order {
 
     public void addDiscount(Integer discount){
         this.total -= discount;
+    }
+
+    public String friendlyTotal(){
+        return String.format("Thanks! The total value of your order is £%d.", this.total);
     }
 
     // Getter methods:
