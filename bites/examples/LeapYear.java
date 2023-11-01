1632e0b13e04600779b28cc71916094e45609c40
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-01 16:51:56 +0000

    Add closetLeapYear()

--- bites/examples/LeapYear.java
+++ bites/examples/LeapYear.java
@@ -1,4 +1,5 @@
 package bites.examples;
+import java.util.ArrayList;
 
 public class LeapYear {
     public static void main(String[] args) {
@@ -7,6 +8,8 @@ public class LeapYear {
         System.out.println(LeapYear.isLeapYear(1900));
         System.out.println(LeapYear.isLeapYear(1988));
         System.out.println(LeapYear.isLeapYear(1500));
+        System.out.println(LeapYear.listLeapYears(2000, 2008));
+        System.out.println(LeapYear.closestLeapYear(2013));
     }
 
     public static Boolean isLeapYear(Integer year) {
@@ -16,4 +19,27 @@ public class LeapYear {
             return year % 4 == 0 && year % 100 != 0;
         }
     }
+
+    public static ArrayList<Integer> listLeapYears(Integer startYear, Integer endYear) {
+        ArrayList<Integer> leapYears = new ArrayList<>();
+        for (Integer i = startYear; i <= endYear; i++) {
+            if (LeapYear.isLeapYear(i)) {
+                leapYears.add(i);
+            }
+        }
+        return leapYears;
+    }
+
+    public static Integer closestLeapYear(Integer checkYear) {
+        int offset = 0;
+        while (true) {
+            if (LeapYear.isLeapYear(checkYear + offset)) {
+                return checkYear + offset;
+            } else if (LeapYear.isLeapYear(checkYear - offset)) {
+                return checkYear - offset;
+            }
+            offset++;
+        }
+    }
 }
+
