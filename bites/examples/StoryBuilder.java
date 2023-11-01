0a81f1ef475878cf6d13f0837fe98406c728dbd0
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-01 10:29:00 +0000

    Add StoryBuilder

---
+++ bites/examples/StoryBuilder.java
@@ -0,0 +1,26 @@
+package bites.examples;
+
+public class StoryBuilder {
+    // Class attributes:
+    String plot;
+
+    // Constructor:
+    public StoryBuilder(String plot) {
+        this.plot = plot;
+    }
+
+    public static void main(String[] args) {
+        StoryBuilder storyBuilder = new StoryBuilder("Once upon a time,");
+        storyBuilder.addPlotline(" in a land far, far away");
+        System.out.println(storyBuilder.getPlot());
+    }
+
+    public void addPlotline(String content) {
+        this.plot += content;
+    }
+
+    public String getPlot() {
+        return this.plot;
+    }
+
+}
