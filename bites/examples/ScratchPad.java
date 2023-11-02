b9dcee0070817c3461c7aea45a3ddb6f28494311
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-02 17:05:27 +0000

    Add ScratchPad to experiment with HashMaps

---
+++ bites/examples/ScratchPad.java
@@ -0,0 +1,19 @@
+package bites.examples;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ScratchPad {
+    public static void main(String[] args) {
+        HashMap<String, Integer[]> scores = new HashMap<String, Integer[]>();
+
+        scores.put("Puffins", new Integer[] {1, 2, 2});
+        scores.put("Tigers", new Integer[] {1, 2, 2});
+        scores.put("Buffalos", new Integer[] {2, 0, 2});
+
+        System.out.println(scores.get("Puffins")[0]);
+        System.out.println(Arrays.toString(scores.get("Tigers")));
+        System.out.println(Arrays.toString(scores.get("Buffalos")));
+    }
+
+
+}
