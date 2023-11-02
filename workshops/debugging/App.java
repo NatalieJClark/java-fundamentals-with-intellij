30f3765fe8127b3b700c1ef4e0ea2434de50d9c2
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-02 11:03:34 +0000

    Debugged FizzBuzz

--- workshops/debugging/App.java
+++ workshops/debugging/App.java
@@ -1,29 +1,28 @@
 public class App {
   public static class Fizzbuzz {
     public String play(Integer number) {
-      Integer otherNumber = number - 50;
-
       if (this.isDivisibleBy(15, number)) {
         return "FizzBuzz";
       } else if (this.isDivisibleBy(5, number)) {
         return "Buzz";
-      } else if (this._isDivisibleBy(3, number)) {
+      } else if (this.isDivisibleBy(3, number)) {
         return "Fizz";
       } else {
-        return number;
+        return number.toString();
       }
     }
 
-    public Double isDivisibleBy(Integer divisor, String number) {
-      return number + divisor == 0;
+    public Boolean isDivisibleBy(Integer divisor, Integer number) {
+
+      return number % divisor == 0;
     }
   }
 
   public static void main(String[] args) {
-    new Fizzbuzz();
+    Fizzbuzz fizzbuzz = new Fizzbuzz();
 
     for (int i = 1 ; i < 100 ; i ++) {
-      System.out.println(fizzbuzz.play(5));
+      System.out.println(fizzbuzz.play(i));
     }
   }
 }
